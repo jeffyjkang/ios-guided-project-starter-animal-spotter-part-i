3d1f80d8532423fef3fe34e3409347ac8652b603
Ben Gohlke <jcgohlke@me.com>
2020-05-11 15:27:29 -0400

    Updated formatting

--- AnimalSpotter/Controllers/APIController.swift
+++ AnimalSpotter/Controllers/APIController.swift
@@ -10,19 +10,19 @@ import Foundation
 import UIKit
 
 final class APIController {
-
-enum HTTPMethod: String {
-    case get = "GET"
-    case post = "POST"
-}
-
-enum NetworkError: Error {
-    case noData
-}
-
-private let baseURL = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
-private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
-private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
+    
+    enum HTTPMethod: String {
+        case get = "GET"
+        case post = "POST"
+    }
+    
+    enum NetworkError: Error {
+        case noData
+    }
+    
+    private let baseURL = URL(string: "https://lambdaanimalspotter.vapor.cloud/api")!
+    private lazy var signUpURL = baseURL.appendingPathComponent("/users/signup")
+    private lazy var signInURL = baseURL.appendingPathComponent("/users/login")
     
     // create function for sign up
     
